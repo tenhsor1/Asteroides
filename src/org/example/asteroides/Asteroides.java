83f6b281808c0bc2701f2009f840018eed75b62b
Ricardo Romo Ramirez <ricardo.romo.ramirez@gmail.com>
2013-03-25 13:57:12 -0600

    Cuarto commit: Agregamos Preferencias

--- src/org/example/asteroides/Asteroides.java
+++ src/org/example/asteroides/Asteroides.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -11,41 +12,59 @@ import android.widget.Button;
 public class Asteroides extends Activity {
 	private Button bAcercaDe;
 	private Button bSalir;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        bAcercaDe = (Button) findViewById(R.id.Button03);
-        bAcercaDe.setOnClickListener(new OnClickListener() {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		bAcercaDe = (Button) findViewById(R.id.Button03);
+		bAcercaDe.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				lanzarAcercaDe(null);
-				
+
 			}
 		});
-        bSalir = (Button) findViewById(R.id.Button04);
-        bSalir.setOnClickListener(new OnClickListener() {
+		bSalir = (Button) findViewById(R.id.Button04);
+		bSalir.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				finish();
-				
+
 			}
 		});
-    }
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		super.onCreateOptionsMenu(menu);
+		getMenuInflater().inflate(R.menu.menu, menu);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.asteroides, menu);
+		return true;
+	}
 
-        return true;
-    } 
-    
-    public void lanzarAcercaDe(View view){
-    	
-    	Intent i = new Intent(this, AcercaDe.class);
-    		startActivity(i);
-    }
-    
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.acercaDe:
+			lanzarAcercaDe(null);
+			break;
+		}
+		return true;
+		/** true -> consumimos el item, no se propaga */
+	}
+
+	public void lanzarAcercaDe(View view) {
+
+		Intent i = new Intent(this, AcercaDe.class);
+		startActivity(i);
+	}
+	
+	public void lanzarPreferencias(View view) {
+
+		Intent i = new Intent(this, Preferencias.class);
+		startActivity(i);
+	}
+
 }
