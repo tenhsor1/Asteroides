f95832cfbb6617e40b90c8f5b55deab23bbb2698
Ricardo Romo Ramirez <ricardo.romo.ramirez@gmail.com>
2013-03-25 12:32:33 -0600

    Cuarto commit: Agregamos Acercade

--- gen/org/example/asteroides/R.java
+++ gen/org/example/asteroides/R.java
@@ -24,16 +24,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Button01=0x7f080000;
-        public static final int Button02=0x7f080001;
-        public static final int Button03=0x7f080002;
-        public static final int Button04=0x7f080003;
-        public static final int action_settings=0x7f080006;
-        public static final int tableRow1=0x7f080004;
-        public static final int tableRow2=0x7f080005;
+        public static final int Button01=0x7f080001;
+        public static final int Button02=0x7f080002;
+        public static final int Button03=0x7f080003;
+        public static final int Button04=0x7f080004;
+        public static final int TextView01=0x7f080000;
+        public static final int action_settings=0x7f080007;
+        public static final int tableRow1=0x7f080005;
+        public static final int tableRow2=0x7f080006;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int acercade=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class menu {
         public static final int asteroides=0x7f070000;
@@ -42,6 +44,7 @@ public final class R {
         public static final int Acercade=0x7f050005;
         public static final int Arrancar=0x7f050003;
         public static final int Configurar=0x7f050004;
+        public static final int ContentAcerca=0x7f050008;
         public static final int Salir=0x7f050006;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
@@ -75,5 +78,8 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int TemaAsteroides=0x7f060004;
+        public static final int TextoAsteroides=0x7f060002;
+        public static final int TextoAsteroides_Botones=0x7f060003;
     }
 }
--- src/org/example/asteroides/Asteroides.java
+++ src/org/example/asteroides/Asteroides.java
@@ -1,15 +1,36 @@
 package org.example.asteroides;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Asteroides extends Activity {
-
+	private Button bAcercaDe;
+	private Button bSalir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        bAcercaDe = (Button) findViewById(R.id.Button03);
+        bAcercaDe.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				lanzarAcercaDe(null);
+				
+			}
+		});
+        bSalir = (Button) findViewById(R.id.Button04);
+        bSalir.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+				
+			}
+		});
     }
 
 
@@ -17,7 +38,14 @@ public class Asteroides extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.asteroides, menu);
+
         return true;
+    } 
+    
+    public void lanzarAcercaDe(View view){
+    	
+    	Intent i = new Intent(this, AcercaDe.class);
+    		startActivity(i);
     }
     
 }
